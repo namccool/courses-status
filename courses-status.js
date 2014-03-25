0d11270069d5fc21d8ef1b7efa31e2d10f9e1c72
Nellie Ann McCool <namccool@gmail.com>
2014-03-24 22:34:02 -0700

    externalize stylesheet

--- courses-status.js
+++ courses-status.js
@@ -2,12 +2,18 @@ var body = $("body")
 var button = $("button")
 
 $(document).ready(function(){
+
 	$(".status").on("click", function(){
 		var message = $("<span>Excellent Progress!</span>");
 		$(this).after(message);
 		$(this).remove();
 	});
-	$("#filters").on("click", ".recommended", function(){
-		$(".course").filter(".recommended").css("background-color", "yellow");
+
+	var filters = $("#filters");
+	var course = $(".course");
+
+	filters.find(".recommended").click(function(){
+		course.filter(".recommended").css("background-color", "yellow");
 	});
+
 });
